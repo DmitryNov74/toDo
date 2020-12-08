7874a41b355634cc7ad70c16d05780f42531a47a
Dmitry Nikitin <demetrius7@gmail.com>
2020-12-08 11:43:52 +0200

    buttons
    tryed to add complete,active ,all buttons

--- app.js
+++ app.js
@@ -2,15 +2,30 @@
 var input = document.querySelector(".block_form-input");
 var btn = document.querySelector(".block_form-btn");
 var blocklist = document.querySelector(".block_list");
+var btnActive = document.getElementById('active');
+var btnAll = document.getElementById('all');
+var btnCompleted= document.getElementById('completed');
+
+var todos = [];
+var elementId = 0;
 
 function createElements(){
     var newElem = document.createElement("h3");
+    newElem.id = elementId;
+    elementId += 1;
+
+    todos.append({element:newElem, status:'incomplete'});
     var deletSpan = document.createElement("span");
     var doneBox = document.createElement("input");
     doneBox.type = "checkbox";
 
     doneBox.className = "block_list-done";
-    doneBox.addEventListener("onclick",function(){
+    doneBox.addEventListener("change",function(e){
+        if(e.target.checked){
+            alert('checked');
+        }else{
+            alert('not checked');
+        }
         newElem.className = "block_list-elem.checked";
         
            
@@ -40,4 +55,14 @@ btn.addEventListener("click",function(e){
     }else{
         alert("Please write your task!");
     }
+
+btnCompleted.addEventListener('click',function(e){
+    e.preventDefault();
+    todos.filter(function(completed){
+        createElements(input.value);
+        input.value = "";
+    });
+});
+
+
 });
